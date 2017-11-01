b1f214f5cbd3732c579fe3d59ae5df67c3b9c12b
Jason <jay45_55@hotmail.com>
2017-11-01 13:32:39 -0400

    update register view and model

--- group5/Models/AccountViewModels/RegisterViewModel.cs
+++ group5/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,22 @@ namespace group5.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
