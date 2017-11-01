c6fd34bd702b4fbce02e5993a567b0b3b693f7d7
Jason <jay45_55@hotmail.com>
2017-11-01 11:33:42 -0400

    Add project files.

---
+++ group5/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace group5.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
