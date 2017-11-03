c301b9872c5c30b575a632a3f574de751fa78285
Jason <jay45_55@hotmail.com>
2017-11-03 09:52:29 -0400

    update css and Gallery view

---
+++ group5/Models/UserModels/user.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace group5.Models.UserModels
+{
+    public class user
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string phone { get; set; }
+        public string Password { get; set; }
+
+    }
+}
